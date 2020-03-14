402df57a08bf752563fb27188c386c02198143a0
y_shirahama <yuuya-shirahama@baby-calendar.jp>
2020-03-15 04:14:05 +0900

    MySQLへの接続設定を追記

--- django_myweb/settings.py
+++ django_myweb/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'django_myweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_db',
+        'USER': 'vagrant',
+        'PASSWORD': 'vagrant',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
@@ -104,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
